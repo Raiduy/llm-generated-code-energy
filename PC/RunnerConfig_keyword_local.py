6373012f03726bba0420b48cdfa2494d20d40a70
Raiduy <radu.apsan@gmail.com>
2024-11-18 15:37:47 +0100

    Experiment checkpoint

--- PC/RunnerConfig_keyword_local.py
+++ PC/RunnerConfig_keyword_local.py
@@ -38,7 +38,6 @@ class RunnerConfig:
     
     global ssh_client
 
-
     # Dynamic configurations can be one-time satisfied here before the program takes the config as-is
     # e.g. Setting some variable based on some criteria
     def __init__(self):
@@ -94,6 +93,9 @@ class RunnerConfig:
         ssh_client=paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(hostname=NI_1, username=USERNAME, password=PASSWORD)
+        stdin, stdout, stderr = ssh_client.exec_command('sudo -S energibridge sleep 3')
+        stdin.write(f'{PASSWORD}\n')
+        print(stdout.readlines())
 
 
 
