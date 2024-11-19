75ec6c56f1170c8fffc89bb6765e39c3b3461c03
Raiduy <radu.apsan@gmail.com>
2024-11-19 16:01:15 +0100

    Merges and merges

---
+++ PC/guideline/code/gpt-4/79.py
@@ -0,0 +1,39 @@
+
+
+def decimal_to_binary(decimal, padding_length):
+    # Check if inputs are valid
+    if not isinstance(decimal, int) or not isinstance(padding_length, int) or decimal < 0 or padding_length < 0:
+        return "Invalid Input"
+
+    # Convert decimal to binary
+    binary = bin(decimal)[2:]
+
+    # Check if padding length is sufficient
+    if len(binary) > padding_length:
+        return "Padding length is too small"
+
+    # Pad binary with '0's at the beginning
+    binary = binary.zfill(padding_length)
+
+    # Add extra characters at the beginning and end
+    binary = "db" + binary + "db"
+
+    return binary
+
+
+if __name__ == '__main__':
+    inputs = [[15, 5], [32, 5], [32, 10], [0, 5], [1, 1], [255, 8], [1024, 5], [500, 10], [1023, 10], [-10, 2], [10, -2], [10, 0], ['string', 5], [10, 'string'], [0.5, 1], [True, 1], [10, True], [1, 1], [2, 2], [7, 3], [100, 7], [255, 8], [1023, 10], [2047, 11], [0, 1], [31, 1], [200, 4], [999, 20], [-5, 10], [10, -5], ['abc', 10], [10, 'def'], [None, 10], [10, None], [[], 10], [10, []], [{}, 10], [10, {}], [0, 5], [1, 1], [2, 1], [32, 6], [128, 8], [255, 8], [500, 10], [-1, 5], [15, -1], ['15', 5], [15, '5'], [None, 5], [15, None], [0, 5], [1, 5], [255, 8], [1023, 10], [1000, 20], [150, 12], [-5, 5], [15, -5], ['15', 5], [15, '5'], [[], 5], [15, []], [0, 5], [1, 2], [2, 2], [10, 2], [10, 5], [255, 10], [255, 15], [1024, 20], [123456, 20], [-1, 5], [15, -1], [15, 0], ['15', 5], [15, '5'], [True, 5], [None, 5], [5.5, 5], [5, 5.5], [[15], 5], [15, [5]]]
+    number_of_executions = 13270731
+    inputs_len = len(inputs)
+
+    execution_counter = 0
+    inputs_counter = 0
+
+    while execution_counter != number_of_executions:
+        a = decimal_to_binary(*inputs[inputs_counter])
+        inputs_counter += 1
+
+        if inputs_counter == inputs_len:
+            inputs_counter = 0
+            execution_counter += 1
+
